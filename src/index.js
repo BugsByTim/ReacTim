5c3a51739909c19fe5f37d3bba4d036d9411ffec
Tim Baldwin <timwb499@icloud.com>
2025-01-09 10:54:59 -0600

    ReacTim version 0 is complete. I have completed the tutorial by Rodrigo Pombo. I am going to refactor this app now. Then I will try and expand upon it and make it better.

--- src/index.js
+++ src/index.js
@@ -29,12 +29,7 @@ function createDom(fiber) {
           ? document.createTextNode("")
           : document.createElement(fiber.type)
   
-  const isProperty = key => key !== "children"
-  Object.keys(fiber.props)
-      .filter(isProperty)
-      .forEach(name => {
-          dom[name] = fiber.props[name]
-      })
+  updateDom(dom, {}, fiber.props)
 
   return dom
 }
@@ -184,9 +179,43 @@ function performUnitOfWork(fiber) {
   }
 }
 
+let wipFiber = null
+let hookIndex = null
+
 function updateFunctionComponent(fiber) {
+  wipFiber = fiber
+  hookIndex = 0
+  wipFiber.hooks = []
   const children = [fiber.type(fiber.props)]
   reconcileChildren(fiber, children)
+}
+
+function useState(initial) {
+  const oldHook = wipFiber.alternate && wipFiber.alternate.hooks && wipFiber.alternate.hooks[hookIndex]
+  const hook = {
+    state: oldHook ? oldHook.state : initial,
+    queue: [],
+  }
+
+  const actions = oldHook ? oldHook.queue : []
+  actions.forEach(action => {
+    hook.state = action(hook.state)
+  })
+
+  const setState = action => {
+    hook.queue.push(action)
+    wipRoot = {
+      dom: currentRoot.dom,
+      props: currentRoot.props,
+      alternate: currentRoot,
+    }
+    nextUnitOfWork = wipRoot
+    deletions = []
+  }
+
+  wipFiber.hooks.push(hook)
+  hookIndex++
+  return [hook.state, setState]
 }
 
 function updateHostComponent(fiber) {
@@ -232,6 +261,9 @@ function reconcileChildren(wipFiber, elements) {
       oldFiber.effectTag = "DELETION"
       deletions.push(oldFiber)
     }
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling
+    }
 
     if (index === 0) {
       wipFiber.child = newFiber
@@ -246,13 +278,24 @@ function reconcileChildren(wipFiber, elements) {
 
 const Reactim = {
   createElement,
-  render
+  render,
+  useState,
 }
 
 /** @jsx Reactim.createElement */
+function Counter() {
+  const [state, setState] = Reactim.useState(1)
+  return (
+    <h1 onClick={() => setState(c => c + 1)}>
+      Count: {state}
+    </h1>
+  )
+}
+
 function App(props) {
   return <h1>Hello {props.name}!</h1>
 }
-const element = <App name="World" />
+
+const element = <Counter />
 const container = document.getElementById("root")
 Reactim.render(element, container)
